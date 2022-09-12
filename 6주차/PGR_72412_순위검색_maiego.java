74b38d62fc1b766acd50f54115b04f058a0f0337
SeungRyongHa <maiego@naver.com>
2022-09-12 13:23:44 +0900

    Update PGR_72412_순위검색_maiego.java

--- 6주차/PGR_72412_순위검색_maiego.java
+++ 6주차/PGR_72412_순위검색_maiego.java
@@ -1,66 +1,60 @@
-// 효율성 실패
-// score빼고 네가지 조건중 하나 고른걸로 set을 만들고 score순 정렬을 하면
-// O(쿼리 수 * log 사람수) 가 될 거 같음
-
 import java.util.*;
-import java.util.regex.Pattern;
-    
+
 class Solution {
-    static int n;
-    
-    static class Item {
-        String s;
-        int score;
-        Item(String a, int b) {
-            s=a; score=b;
-        }
+    static Map<String, List<Integer>> group = new HashMap<>();
+
+    static void add(String k, int v) {
+        List<Integer> arr = group.getOrDefault(k, new ArrayList<>());
+        arr.add(v);
+        group.put(k, arr);
     }
-    
+
     public int[] solution(String[] info, String[] query) {
-        n = info.length;
-        Item[] arr = new Item[n];
-        for (int i=0; i<n; ++i) {
-            int idx = info[i].lastIndexOf(" ");
-            String s = info[i].substring(0,idx);
-            int score = Integer.parseInt(info[i].substring(idx+1));
-            arr[i] = new Item(s, score);
+        for (String s: info) {
+            int idx = s.lastIndexOf(" ");
+            int score = Integer.parseInt(s.substring(idx+1));
+
+            String[] splitted = s.substring(0,idx).split(" ");
+            for (int subset=0; subset<(1<<4); ++subset) {
+                StringBuilder k = new StringBuilder();
+                for (int i=0; i<4; ++i) {
+                    if ((subset&(1<<i)) > 0) {
+                        k.append(splitted[i]).append(" ");
+                    } else k.append("- ");
+                }
+                add(k.toString(), score);
+            }
         }
-        Arrays.sort(arr, (a,b)->b.score-a.score);
-        
+
+        for (String k: group.keySet())
+            Collections.sort(group.get(k));
+
+
         int[] answer = new int[query.length];
         int queryIdx = 0;
-        for (String q: query) {
-            StringBuilder regPat = new StringBuilder();
-            String[] input = q.split(" ");
-            if (!input[0].equals("-"))
-                regPat.append(input[0]);
-            else regPat.append("(cpp|java|python)");
-            regPat.append(" ");
-            
-            if (!input[2].equals("-"))
-                regPat.append(input[2]);
-            else regPat.append("(backend|frontend)");
-            regPat.append(" ");
-            
-            if (!input[4].equals("-"))
-                regPat.append(input[4]);
-            else regPat.append("(junior|senior)");
-            regPat.append(" ");
-            
-            if (!input[6].equals("-"))
-                regPat.append(input[6]);
-            else regPat.append("(chicken|pizza)");
-            
-            int score = 0;
-            if (!input[7].equals("-"))
-                score = Integer.parseInt(input[7]);
-            int cnt=0;
-            for (int i=0; i<n; ++i) {
-                if (arr[i].score<score) break;
-                if (Pattern.matches(regPat.toString(), arr[i].s))
-                    ++cnt;
+        for (String s: query) {
+            s = s.replaceAll(" and ", " ");
+            int idx = s.lastIndexOf((" "));
+            int score = Integer.parseInt(s.substring(idx+1));
+            String k = s.substring(0,idx+1);
+
+            List<Integer> arr = group.get(k);
+            if (arr==null) {
+                answer[queryIdx++] = 0;
+                continue;
             }
-            answer[queryIdx++] = cnt;
+
+            idx = -1;
+            int lo=0, hi=arr.size()-1;
+            while (lo<=hi) {
+                int mid = lo+hi>>1;
+                if (arr.get(mid)<score) {
+                    lo = mid+1;
+                    idx = Math.max(idx, mid);
+                }
+                else hi = mid-1;
+            }
+            answer[queryIdx++] = arr.size()-1-idx;
         }
         return answer;
     }
